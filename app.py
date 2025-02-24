ff4bf8be7a74cd0c2a7260e11da9c5d328b01a13
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2025-02-24 15:25:15 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_error, mean_absolute_percentage_error
 from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
-from utils import plot_forecast
+from utils import plot_forecast, plot_errors, plot_errors_dist
 
 st.set_page_config(
     page_title="Prophet",
@@ -45,14 +45,36 @@ with st.sidebar:
                 help="""The proportion of observations that will be used for model selection.
                 Remember to retain a separate hidden test set for final model evaluation."""
             )
+            # split data
+            df.sort_values('ds', ascending=True, inplace=True) 
+            df_train, df_val = train_test_split(df, test_size=val_size, shuffle=False)
+            df_train['set'] = 'train'
+            df_val['set'] = 'validation'
+            df_split = pd.concat([df_train, df_val])
+    with st.expander("Trend Parameters"):
+        if uploaded_file is not None:
+            growth = st.radio("Trend Type", ['linear', 'logistic'])
+            if growth == 'logistic':
+                cap = st.number_input("Maximum", value=2 * df_train['y'].max())
+                floor = st.number_input("Minimum", value=0)
+                df['cap'] = cap 
+                df['floor'] = floor 
+                df_train['cap'] = cap 
+                df_train['floor'] = floor 
+                df_val['cap'] = cap 
+                df_val['floor'] = floor
+            changepoint_selection = st.radio("Changepoint Selection", ['automatic', 'manual'])
+            if changepoint_selection == 'automatic':
+                n_changepoints = st.number_input("Number of Trend Changepoints", min_value=0, max_value=50, value=25)
+                changepoint_range = st.slider("Range of Trend Changepoints", min_value=0.0, max_value=1.0, value=0.8, step=0.05)
+                changepoints=None
+            elif changepoint_selection == 'manual':
+                n_changepoints = None
+                changepoint_range = 0.8
+                changepoints = st.multiselect("Changepoints", df_train['ds'].unique())
+            changepoint_prior_scale = st.slider("Changepoint Prior Strength", min_value=0.01, max_value=1.0, value=0.05, step=0.01)
 
 if uploaded_file is not None:
-    # split data
-    df.sort_values('ds', ascending=True, inplace=True) 
-    df_train, df_val = train_test_split(df, test_size=val_size, shuffle=False)
-    df_train['set'] = 'train'
-    df_val['set'] = 'validation'
-    df_split = pd.concat([df_train, df_val])
     split_fig = px.line(
         df_split,
         x='ds',
@@ -63,7 +85,13 @@ if uploaded_file is not None:
     st.plotly_chart(split_fig)   
 
     # define and fit Prophet model 
-    m = Prophet()
+    m = Prophet(
+        growth=growth,
+        n_changepoints=n_changepoints,
+        changepoint_range=changepoint_range,
+        changepoint_prior_scale=changepoint_prior_scale,
+        changepoints=changepoints
+    )
     m.fit(df_train)
     forecast = m.predict(df)
 
@@ -78,11 +106,13 @@ if uploaded_file is not None:
         on='ds',
         how='left'
     )
-    forecast_fig = plot_forecast(df_merged)
     st.header("Forecast")
+    show_changepoints = True
+    forecast_fig = plot_forecast(m, df_merged, show_changepoints=show_changepoints)
     st.plotly_chart(forecast_fig)
 
     # evaluation 
+    df_merged['error'] = df_merged['y'] - df_merged['yhat']
     y_true = df_merged.query("set == 'validation'")['y']
     y_pred = df_merged.query("set == 'validation'")['yhat']
     rmse = root_mean_squared_error(y_true, y_pred)
@@ -90,5 +120,9 @@ if uploaded_file is not None:
     col1, col2 = st.columns(2)
     with col1:
         st.metric(label='Root Mean Sqared Error', value=f'{round(rmse, 2)}')
+        errors_fig = plot_errors(df_merged.query("set == 'validation'"))
+        st.plotly_chart(errors_fig)
     with col2:
         st.metric(label='Mean Absolute Percentage Error', value=f'{round(100 * mape, 2)}%')
+        errors_dist_fig = plot_errors_dist(df_merged.query("set == 'validation'"))
+        st.plotly_chart(errors_dist_fig)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import plotly.express as px 
 
-def plot_forecast(forecast):
+def plot_forecast(model, forecast, show_changepoints=False):
     fig = px.line(
         forecast,
         x='ds',
@@ -30,5 +30,26 @@ def plot_forecast(forecast):
         line=dict(dash='dash'),
         marker=dict(color='gray')
     )
+    if show_changepoints:
+        for cp, delta in zip(model.changepoints, model.params['delta'][0]):
+            if abs(delta) > 0.1:
+                fig.add_vline(x=cp, line_dash="dash", line_color="red", opacity=0.6)
     fig.update_layout(hovermode='x')
+    return fig 
+
+def plot_errors(df):
+    fig = px.bar(
+        df,
+        x='ds',
+        y='error',
+        title='Forecast Errors'
+    )
+    return fig 
+
+def plot_errors_dist(df):
+    fig = px.histogram(
+        df,
+        x='error',
+        title='Error Distribution'
+    )
     return fig 
